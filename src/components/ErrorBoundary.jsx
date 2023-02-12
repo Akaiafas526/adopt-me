cdcd3b02d4d7f611e2a6920c2de5531866fca90c
Andreas Kaiafas <akaiafas526@gmail.com>
2023-02-12 16:58:26 -0500

    Re worked eror boundary

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
+// mostly code from reactjs.org/docs/error-boundaries.html
 import { Component } from "react";
+import { Link } from "react-router-dom";
 
 class ErrorBoundary extends Component {
   state = { hasError: false };
@@ -8,14 +8,14 @@ class ErrorBoundary extends Component {
     return { hasError: true };
   }
   componentDidCatch(error, info) {
-    console.error("ErrorBoundary caught on error", error, info);
+    console.error("ErrorBoundary caught an error", error, info);
   }
   render() {
     if (this.state.hasError) {
       return (
         <h2>
           There was an error with this listing. <Link to="/">Click here</Link>{" "}
-          to go back to the homepage.
+          to back to the home page.
         </h2>
       );
     }

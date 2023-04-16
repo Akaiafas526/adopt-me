9ab618a8b48fca0d3c6472b862f77d0d6b8b6e41
Andreas Kaiafas <akaiafas526@gmail.com>
2023-04-16 13:09:10 -0400

    fixed adopt search function, installed tailwind and deleted current css styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Details from "./Details";
+import { useState } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const queryClient = new QueryClient({
@@ -18,19 +18,26 @@ const queryClient = new QueryClient({
 const App = () => {
   const adoptedPet = useState(null);
   return (
-    <div>
+    <div
+      className="m-0 p-0"
+      style={{
+        background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+      }}
+    >
       <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <AdoptedPetContext.Provider value={adoptedPet}>
-            <header>
-              <Link to="/">Adopt Me!</Link>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <QueryClientProvider client={queryClient}>
+            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+              <Link className="text-6xl text-white hover:text-gray-200" to="/">
+                Adopt Me!
+              </Link>
             </header>
             <Routes>
               <Route path="/details/:id" element={<Details />} />
               <Route path="/" element={<SearchParams />} />
             </Routes>
-          </AdoptedPetContext.Provider>
-        </QueryClientProvider>
+          </QueryClientProvider>
+        </AdoptedPetContext.Provider>
       </BrowserRouter>
     </div>
   );
